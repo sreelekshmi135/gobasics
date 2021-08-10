566dfda42c9d80d78da2890317e14bcc42f5d750
sreelekshmi135 <48550847+sreelekshmi135@users.noreply.github.com>
2021-08-10 11:44:23 +0530

    add go

--- add.go
+++ add.go
@@ -2,11 +2,10 @@ package main
 
 import "fmt"
 
-func swap(x, y string) (string, string) {
-	return y, x
+func add(x, y int) int {
+	return x + y
 }
 
 func main() {
-	a, b := swap("hello", "world")
-	fmt.Println(a, b)
+	fmt.Println(add(52, 53))
 }
